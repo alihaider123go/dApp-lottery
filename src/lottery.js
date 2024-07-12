705d441fa589b350cce56a00024d8557c5fedc28
alihaider123go <alihaider123go@gmail.com>
2024-07-12 16:13:55 +0500

    basic UI with lottery logic

---
+++ src/lottery.js
@@ -0,0 +1,8 @@
+import web3 from "./web3";
+
+const address = '0x9198464C3d8318a106150Fe2ce98780e58caC092';
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+const lottery = new web3.eth.Contract(abi,address);
+
+export default lottery;
